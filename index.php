a4a46181d2080f2c57b78c61c7f65c310a5b0067
tanaka aika <>
2024-01-26 12:59:32 +0900

    Change fileName

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php require_once('user.php') ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>ショッピングサイト | ログイン</title>
+        <link rel=”icon” type=”image/png” href=“/images/favicon.png”>
+    </head>
+    <body>
+
+    </body>
+</html>
