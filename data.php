50cabf867f87f544d603c94ca7754e4b4b7b690f
tanaka aika <>
2024-01-26 12:47:10 +0900

    Create userInstanse

---
+++ data.php
@@ -0,0 +1,8 @@
+<?php 
+    require_once('user.php');
+
+    $user1 = new User('たかし', 'takashi@mail.com', 'takashi', 'takashi_icon.png');
+    $user2 = new User('ももこ', 'momoko@mail.com', 'momoko', 'momoko_icon.png');
+    $user3 = new User('じろう', 'jiro@mail.com', 'jiro', 'jiro_icon.png');
+    $user4 = new User('ゆきみ', 'yukimi@mail.com', 'yukimi', 'yukimi_icon.png');
+?>
